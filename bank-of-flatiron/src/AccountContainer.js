ccdc5bc8076e31adeae31a00068a32ce069a34e9
git_Vickyprinz <kariukivictor315@gmail.com>
2023-01-15 21:45:02 +0300

    fetch

--- bank-of-flatiron/src/AccountContainer.js
+++ bank-of-flatiron/src/AccountContainer.js
@@ -8,4 +8,19 @@ function AccountContainer() {
           setTransactions(data);
         });
     }, []);
+
+    function handleFormSubmission(newtransactioninput){
+        console.log(newtransactioninput)
+        const configurationData =  {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newtransactioninput),
+        }
+        fetch("http://localhost:3000/transactions",configurationData)
+         .then((response)=>response.json())
+           .then(newItemTRansacted=>setTransactions(transactions=>[...transactions,newItemTRansacted]))
+             .catch((error)=>{console.log(error)})
+  }
 }
