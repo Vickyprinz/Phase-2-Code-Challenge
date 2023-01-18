b7219b24f6c27be132ee6b9e71e7053c4927f79a
victor kariuki <103563265+Vickyprinz@users.noreply.github.com>
2023-01-18 10:07:21 +0300

    updated app.js

--- bank-of-flatiron/src/App.js
+++ bank-of-flatiron/src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import AccountContainer from "./AccountContainer";
 
 function App() {
- 
-   
+
   return (
     <div className="ui raised segment">
-      <div className="ui segment violet inverted">
-        <h2>The Royal Bank of Flatiron</h2>
+      <div className="ui segment  inverted">
+        <h2>Bank of Flatiron</h2>
       </div>
       <AccountContainer />
     </div>
