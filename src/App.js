fc739ac1734b13089aa5c7fcb820e446098f500d
victor kariuki <103563265+Vickyprinz@users.noreply.github.com>
2023-01-18 10:39:22 +0300

    updated

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AccountContainer from "./AccountContainer";
+
+function App() {
+
+  return (
+    <div className="ui raised segment">
+      <div className="ui segment  inverted">
+        <h2>Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
+    </div>
+  );
+}
+
+export default App;
